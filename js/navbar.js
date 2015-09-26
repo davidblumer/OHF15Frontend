fb386d92462be613cda1e1128a6015a089ac0a59
David Blumer <david.blumer@socialbit.de>
2015-09-26 22:28:48 +0200

    added filter buttons

--- js/navbar.js
+++ js/navbar.js
@@ -5,18 +5,22 @@ $(document).ready(function ()
             $(this).removeClass('filter');
             $('.navbar,.wrapper').removeClass('filter');
             $('.refine-search').removeClass('active');
+            $('.filter-divider').removeClass('active');
             $('.divider').removeClass('filter2');
             $('#search').removeClass('glyphicon-remove').addClass('glyphicon-search');
         }        else {
             $(this).addClass('filter');
             $('.navbar,.wrapper').addClass('filter');
             $('.refine-search').addClass('active');
+            $('.filter-divider').addClass('active');
             $('.divider').addClass('filter2');
             $('#search').removeClass('glyphicon-search').addClass('glyphicon-remove');
         }
-        $("#home").click(function (e) {
+        $("#home").click(function () {
             $('.navbar,.wrapper').removeClass('filter');
             $('.refine-search').removeClass('active');
+            $('#search').removeClass('glyphicon-remove').addClass('glyphicon-search');
+            $('.divider').removeClass('filter2');
         });
     });
 });
